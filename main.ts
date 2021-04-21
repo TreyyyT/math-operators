f2212b35b58ae82cd529f2248836d974eab4e5c5
TreyyyT <82166923+TreyyyT@users.noreply.github.com>
2021-04-20 23:57:34 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let d = ""
+let b = 0
+let a = game.askForNumber("Give me a military time(hour)", 2)
+if (a > 12) {
+    b = a % 12
+    d = "p.m."
+} else if (a == 12) {
+    b = 12
+    d = "p.m."
+} else if (a == 0) {
+    b = 12
+    d = "a.m."
+} else {
+    b = a
+    d = "a.m."
+}
+let c = game.askForNumber("Give me a military time(minutes)", 2)
+game.splash("The time is " + b + ":" + c + " " + d)
